f452cde21261641e8843aad8ea81d502fbcbf523
bruno <bpires.siqueira@gmail.com>
2022-10-09 18:55:36 -0300

    validador de formulario

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,3 +41,40 @@ plus.forEach((item) => {
     item.addEventListener('click', ativarCarrinho);
     item.addEventListener('touchstart', ativarCarrinho);
 })
+
+
+
+//  VALIDAR FOMULÁRIO
+
+
+function validador() {
+    let form = document.getElementById('form');
+    let email = document.getElementById('email').value;
+    let texto = document.getElementById('validador-texto');
+    let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+    if (email.match(pattern)) {
+        form.classList.add('valido')
+        texto.innerHTML = 'Email válido!';
+        texto.style.color = "#4caf50";
+    }
+    else {
+        form.classList.remove('valido');
+        texto.innerHTML = 'Email inválido!'
+        texto.style.color = "#f44336";
+    }
+}
+
+// BOTÃO DE ENVIAR QUE DISPARA UM ALERTA AO CLICK OU TOUCH
+
+const formEnviar = document.querySelector('.form-btn');
+const sucess = document.querySelector('.form-sucesso');
+
+function formEnviado(event) {
+    if (event.type === 'touchstart') event.preventDefault();
+    sucess.innerHTML = 'Enviado com sucesso!'
+    sucess.style.color = "#4caf50";
+}
+
+formEnviar.addEventListener('click', formEnviado);
+formEnviar.addEventListener('touchstart', formEnviado);
